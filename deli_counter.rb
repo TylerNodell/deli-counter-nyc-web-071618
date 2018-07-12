8c71a63a23ed43722c1b69977a519b1dc69c311b
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 02:45:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
 # Write your code here.
+def line(array)
+  if array.length > 0
+    puts "The line is currently: "
+    array.each_with_index { |name, index| print "#{index+1}. #{name} " }
+  end
+end
