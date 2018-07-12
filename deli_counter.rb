097b6fe0a6cacc9ea92d9e1353ec932b890fa56f
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 02:47:52 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,12 @@ def line(array)
   if array.length > 0
     puts "The line is currently: "
     array.each_with_index { |name, index| print "#{index+1}. #{name} " }
+  else
+    puts "The line is currently empty."
   end
+end
+
+def take_a_number(array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{array.length} in line."
 end
