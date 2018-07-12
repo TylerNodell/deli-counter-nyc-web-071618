70b32edefd38d92ca9feb8caad061d7103c567ed
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-12 02:58:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 def line(array)
   if array.length > 0
-    current_line = "The line is currently: "
-    array.each_with_index { |name, index| current_line << "#{index+1}. #{name} " }
+    current_line = "The line is currently:"
+    array.each_with_index { |name, index| current_line << " #{index+1}. #{name}" }
     puts current_line
   else
     puts "The line is currently empty."
